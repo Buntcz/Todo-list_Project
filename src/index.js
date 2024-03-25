f2b58c642e18d13be3aa59d2e76107e4b617a3de
Buntcz <testtigeryt@gmail.com>
2024-03-26 00:07:18 +0200

    --all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import { projectButton } from "./project_button";
+import { AddProject } from "./project_add";
+
+const Pform = document.getElementById("Project_form");
+
+Pform.addEventListener('submit', function(event) {
+    event.preventDefault();
+    AddProject();
+    Pform.reset();
+    Pform.style.display = "none";
+})
 
 projectButton();
--- src/project_add.js
+++ src/project_add.js
@@ -1,4 +1,7 @@
+import "./style.css"
+
 const myProjects = [];
+const content = document.getElementById("content")
 
 class Project {
     constructor(title) {
@@ -10,9 +13,34 @@ function AddProject() {
     let title = document.getElementById("P_title").value;
     let newProject = new Project(title);
     myProjects.push(newProject);
+    render();
 }
 
 function render() {
     const projectContainer = document.createElement('div');
-    
+    projectContainer.classList.add("projectContainer")
+    content.appendChild(projectContainer);
+    const projects = document.querySelectorAll(".project")
+
+    for(let i = 0; i < myProjects.length; i++) {
+        createProject(myProjects[i]);
+    }
 }
+
+
+
+function createProject(item) {
+   const display = document.querySelector(".projectContainer");
+   const projectCard = document.createElement("div");
+   projectCard.classList.add("project");
+
+   const projectTitle = document.createElement("h3");
+   projectTitle.classList.add("projectTitle")
+    projectTitle.textContent = item.title;
+
+   display.appendChild(projectCard);
+   projectCard.appendChild(projectTitle);
+   
+}
+
+export {AddProject};
