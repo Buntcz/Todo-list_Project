c6295a9087c2991504b9e8b9199d1326b64add56
Buntcz <testtigeryt@gmail.com>
2024-03-26 22:37:16 +0200

    --all

--- src/project_add.js
+++ src/project_add.js
@@ -33,8 +33,28 @@ function createProject(item) {
    projectTitle.classList.add("projectTitle")
     projectTitle.textContent = item.title;
 
+   const removeButton = document.createElement('button');
+   removeButton.classList.add("removeButton");
+   removeButton.textContent = "Delete Project"
+
+   removeButton.addEventListener("click", function() {
+   projectCard.outerHTML = " ";
+   myProjects.splice(myProjects.indexOf(item),  1);
+   
+   })
+   
+   const editButton = document.createElement('button');
+   editButton.classList.add("editButton");
+   editButton.textContent = "Edit button";
+
+   editButton.addEventListener("click", function() {
+    const ProjForm = document.getElementById("Project_form")
+   })
+   
    display.appendChild(projectCard);
    projectCard.appendChild(projectTitle);
+   projectCard.appendChild(removeButton);
+   projectCard.appendChild(editButton)
    
 }
 
